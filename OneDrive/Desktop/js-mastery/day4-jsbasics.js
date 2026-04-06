c628f803ac9fac8b96215ce5fd5b869ff104251e
perumallavijay <perumallavijay50@gmail.com>
2026-04-07 05:01:20 +0530

    Js Arrays, map, filter, find

---
+++ OneDrive/Desktop/js-mastery/day4-jsbasics.js
@@ -0,0 +1,30 @@
+// 1. Print first skill
+// 2. Print last skill
+// 3. Print total number of skills
+// 4. Add "MongoDB" to the end
+// 5. Remove last skill
+const skills = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+console.log(skills[0]);
+console.log(skills[4]);
+console.log(skills.length);
+skills.push('MongoDB');
+console.log(skills);
+skills.pop();
+console.log(skills);
+//
+
+const jobs = [
+  { title: "React Developer", salary: 60000, location: "Hyderabad" },
+  { title: "Node.js Developer", salary: 35000, location: "Bangalore" },
+  { title: "Frontend Developer", salary: 50000, location: "Hyderabad" },
+  { title: "Backend Developer", salary: 25000, location: "Remote" },
+];
+
+const titles = jobs.map(job => job.title)
+console.log(titles);
+
+const goodJobs = jobs.filter(job => job.salary >= 40000);
+console.log(goodJobs);
+
+const jobLocation = jobs.find(job => job.location === "Hyderabad");
+console.log(jobLocation);
