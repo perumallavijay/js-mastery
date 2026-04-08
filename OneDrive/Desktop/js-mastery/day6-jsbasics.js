9bfd79fe9b6626ccac97c2ef207f79c50bf7f26d
perumallavijay <perumallavijay50@gmail.com>
2026-04-09 00:04:28 +0530

    dom manipulation basics

--- OneDrive/Desktop/js-mastery/day6-jsbasics.js
+++ OneDrive/Desktop/js-mastery/day6-jsbasics.js
@@ -4,10 +4,17 @@ console.log(containerElement);
 const HeadElement = document.createElement('h1');
 
 HeadElement.textContent = "Hello welcome Vijay Perumalla";
+HeadElement.style.color = "Blue"
 console.log(HeadElement);
 
 const textButtonEl = document.createElement('button')
 textButtonEl.textContent = "Hey Yay!"
+textButtonEl.style.color ="#EDDD53"
+
+textButtonEl.addEventListener("click", function() {
+  HeadElement.textContent = "Button was clicked! 🎉";
+  HeadElement.style.color = "blue";
+});
 console.log(textButtonEl)
 
 containerElement.appendChild(HeadElement);
